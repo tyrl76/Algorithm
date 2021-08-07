90477cb7b94f54a717edaca6e61818f33ead9c0d
tyrl76 <tyrl76@gmail.com>
2021-08-08 01:43:33 +0900

    ssh test

--- [카카오 인턴] 키패드 누르기.py
+++ [카카오 인턴] 키패드 누르기.py
@@ -44,13 +44,13 @@ from IPython.display import Image
 #
 # 입출력 예
 #
-# numbers	hand	result
+#         numbers	    hand	   result
 #
-# [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]	"right"	"LRLLLRLLRRL"
+# [1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5]	   "right"	"LRLLLRLLRRL"
 #
-# [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]	"left"	"LRLLRRLLLRR"
+# [7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2]	   "left"	"LRLLRRLLLRR"
 #
-# [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]	"right"	"LLRLLRLLRL"
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]	   "right"	"LLRLLRLLRL"
 #
 # 입출력 예에 대한 설명
 #
